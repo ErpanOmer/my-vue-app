dc9bb1d9a6d43361dc8b80c3c459199b5d6a853d
ErpanOmer <354073823@qq.com>
2023-03-17 18:50:13 +0800

    更新demo

---
+++ src/App15.vue
@@ -0,0 +1,96 @@
+<!-- <template>
+    <div class="fenders-and-kickstand-mobile">
+        <img src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20230307-201732.jpg?v=1678191479">
+        <div style="margin-left: 25px;">
+            <div class="right">
+                <span class="btn">FREE</span>
+                <span style="font-weight: 600;text-decoration: line-through;">$157.00</span>
+            </div>
+            <div class="u17Light">Free Fenders, Kickstand <br> 1-Year eSIM Service Included</div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .fenders-and-kickstand-mobile {
+	  background-color: #F7F8FA;
+	  display: flex;
+      align-items: center;
+	  padding: 10px;
+	  border-radius: 10px;
+    }
+
+    .fenders-and-kickstand-mobile .right {
+        display: flex;
+       align-items: center;
+    }
+
+    .fenders-and-kickstand-mobile img {
+	  width: 80px;
+	}
+
+    .fenders-and-kickstand-mobile .btn { 
+	   padding: 7px 12px;
+	   color: #fff;
+	   background-color: #263B55;
+	   border-radius: 7px;
+	   line-height: 16px;
+       margin-right: 7px;
+       
+    }
+
+    .fenders-and-kickstand-mobile .u17Light {
+	  font-size: 14px;
+      margin-top: 10px;
+    }
+
+    @media (min-width: 768px) {
+        .fenders-and-kickstand-mobile {
+          display: none;
+        }
+    }
+</style> -->
+
+<template>
+<div class="fenders-and-kickstand">
+    <img src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20230307-201732.jpg?v=1678191479"/>
+    <div class="u17Light">Free Fenders, Kickstand, <br> 1-Year eSIM Service Included</div>
+    <div class="price">€128.00</div>
+    <div class="btn">FREE</div>
+</div>
+</template>
+  
+  <style>
+    .fenders-and-kickstand {
+        background-color: #F7F8FA;
+        display: flex;
+       align-items: center;
+          padding: 10px;
+        border-radius: 10px;
+         margin-left: 15%;
+    }
+    
+    .fenders-and-kickstand img {
+        width: 116px;
+      }
+    
+    .fenders-and-kickstand .btn { 
+         padding: 7px 12px;
+         color: #fff;
+         background-color: #263B55;
+         border-radius: 7px;
+         line-height: 16px;
+         margin-left: 20px;
+    }
+    
+    .fenders-and-kickstand .price {
+          text-decoration: line-through;
+        }
+    
+    .fenders-and-kickstand .u17Light {
+        font-size: 18px;
+        line-height: 25px;
+        font-weight: 600;
+        margin: 0 30px 0 20px;
+     }
+  </style>

e359d4f77e0c2b8c3c657ee4489ddf0c71c27391
ErpanOmer <354073823@qq.com>
2023-03-22 19:19:19 +0800

    更新demo

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,172 @@
-<template>
-  <div id="section1">
-    <div class="center">
-      <div>
-        <div class="u17Light">CHORD</div>
-        <div class="u42DemiBold">No One Knows <br> It's Electric</div>
-      </div>
-      <div class="u17Light">
-        Meet the new lightweight city ebike 2023 - the perfect smart electric town bike for urban adventurers. With a
-        range
-        of up to 120km, this ebike is ideal for commuting or leisurely rides around town. With the extra safety and
-        convenience that comes with smart features like anti-theft GPS, onboard navigation and automatic synchronization
-        of
-        ride data, the Chord Ebike is your first step into a new era of cycling.
-      </div>
-    </div>
-  </div>
-</template>
-
 <style>
-#section1 {
-  background-color: #F7F8FA;
-  padding: 100px 0;
+.store-list {
+    display: grid !important;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1%;
+    /* margin: -20px; */
 }
 
-#section1 .center {
-  margin: 0 auto;
-  max-width: 1280px;
-  display: flex;
-  align-items: center;
-
+.store-list .list {
+    max-height: 593px;
+    overflow: auto;
+    padding-right: 10%;
 }
 
-#section1 .center > div {
-  flex: 1;
+.store-list .list .item {
+    padding: 12px 0;
 }
 
-#section1 .center .u17Light {
-  color: #BCBEB9;
-  line-height: 24px;
-  margin-bottom: 10px;
+.store-list .list .item + .item {
+    border-top: 1px solid #ccc;
 }
 
-#section1 .center > div:last-child {
-  color: #000;
+
+
+
+.store-list .list .u30DemiBold {
+    line-height: 50px;
+}
+
+.store-list .list ul {
+    margin: 6px 0 12px;
+    padding: 0;
+    padding-left: 20px;
+}
+
+.store-list .list .u20Medium {
+    margin-bottom: 6px;
+    font-size: 17px;
+    color: #555;
+}
+
+.store-list .list .address {
+    padding-top: 6px;
+    height: 60px;
+}
+
+.store-list .list ul>li::marker {
+    color: #FD4B17;
+}
+
+.store-list .list .my-button {
+    background-color: #FD4B17;
+    padding: 10px 40px;
 }
 
 
 @media (max-width: 767px) {
-  #section1 {
-    padding: 50px 15px;
-  }
+    .store-list {
+        margin: -20px 0 auto;
+        /* grid-template-columns: 1fr; */
+        /* grid-template-rows: 1fr 3fr; */
+        display: flex !important;
+        flex-direction: column;
+    }
 
-  #section1 .center {
-    flex-direction: column;
-    align-items: normal;
-  }
+	.store-list .list {
+        order: 1;
+        padding: 0;
+        flex: 1;
+   }
 
-  #section1 .center .u17Light {
-    font-size: 15px;
-  }
+   .store-list .list .u30DemiBold {
+        font-size: 24px;
+        line-height: 30px;
+        margin-bottom: 12px;
+   }
 
-  #section1 .center .u42DemiBold {
-    font-size: 24px;
-    line-height: 28px;
-    margin-bottom: 24px;
+   .store-list .list .u20DemiBold,
+   .store-list .list .u20Medium {
+        font-size: 16px;
+        line-height: 20px;
+   }
+
+   .store-list .list .my-button {
+        width: 100%;
+        text-align: center;
+   }
+
+   .store-list .list .address {
+     height: auto;
+   }
+
+   .store-list .list ul {
+      margin-bottom: 0;
+   }
   }
-}
 </style>
+
+
+<style>
+  #storepoint-tag-dropdown,
+  #storepoint-search,
+  #storepoint-panel {
+	 display:none!important;
+  }
+  
+  #storepoint-map,
+  #storepoint-container {
+	width: 100% !important;
+    height: 593px !important;
+  }
+
+  /* #storepoint-container .mapboxgl-ctrl-top-right {
+        display: none;
+  } */
+  
+  @media (max-width: 767px) {
+	#storepoint-container,
+    #storepoint-map {
+        max-height: 30vh !important;
+	  /* min-height: calc(100vw - 30px); */
+    }
+  }
+
+  /* @media (min-width: 768px) {
+    #storepoint-container {
+        pointer-events: none;
+    }
+
+    #storepoint-container .stpt-marker {
+        pointer-events: all;
+    }    
+  } */
+</style>
+
+<template>
+    <!-- <div class="hide">
+        <div class="store-list">
+            <div class="list">
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+            </div>
+            <div id="storepoint-container" data-tags="de,Test Ride,Münster" data-map-id="162f21804990ff"></div>
+        </div>
+    </div> -->
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+</script>
---
+++ src/App18.vue
@@ -0,0 +1,75 @@
+<style>
+.shop-detail {}
+
+.shop-detail .u30DemiBold {
+    line-height: 36px;
+    margin-bottom: 12px;
+}
+
+.shop-detail .u20Medium {
+    margin-bottom: 6px;
+    font-size: 17px;
+    color: #555;
+}
+
+.shop-detail ul {
+    margin: 6px 0 12px;
+    padding: 0;
+    padding-left: 20px;
+}
+
+.shop-detail  ul>li::marker {
+    color: #FD4B17;
+}
+
+.shop-detail .item {
+    display: flex;
+    margin-bottom: 6px;
+}
+
+
+.shop-detail label {
+    display: inline-block;
+    min-width: 66px;
+    font-size: 17px;
+}
+
+
+@media (max-width: 767px) {
+
+    .shop-detail .u30DemiBold {
+        font-size: 24px;
+        line-height: 30px;
+        margin-bottom: 12px;
+   }
+
+   .shop-detail .u20DemiBold,
+   .shop-detail .u20Medium {
+        font-size: 16px;
+        line-height: 20px;
+   }
+
+}
+</style>
+
+<template>
+    <div class="shop-detail">
+        <div class="u30DemiBold">ELV Bikes</div>
+        <div class="u20DemiBold">Available Model:</div>
+        <ul>
+            <li class="u20Medium">Carbon One Size M/L</li>
+        </ul>
+        <div class="item">
+            <label class="u20DemiBold">ADD:</label>
+            <span class="u20Medium">425 S. Broadway, Los Angeles, CA 90013</span>
+        </div>
+        <div class="item">
+            <label class="u20DemiBold">Email:</label>
+            <span class="u20Medium">425 S. Broadway, Los Angeles, CA 90013</span>
+        </div>
+        <div class="item">
+            <label class="u20DemiBold">Phone:</label>
+            <span class="u20Medium">425 S. Broadway, Los Angeles, CA 90013</span>
+        </div>
+    </div>
+</template>

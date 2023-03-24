b7936d1c2265f281d6ab18bdc2761194c6ffc30d
ErpanOmer <354073823@qq.com>
2023-03-24 15:00:56 +0800

    更新demo

--- src/App.vue
+++ src/App.vue
@@ -1,172 +1,344 @@
 <style>
-.store-list {
-    display: grid !important;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 1%;
-    /* margin: -20px; */
-}
-
-.store-list .list {
-    max-height: 593px;
-    overflow: auto;
-    padding-right: 10%;
-}
-
-.store-list .list .item {
-    padding: 12px 0;
-}
-
-.store-list .list .item + .item {
-    border-top: 1px solid #ccc;
+#banner {
+    overflow-x: hidden;
+    height: calc(100vh - 111px);
+    /* height: 100vh; */
+    /* min-height: 800px; */
+    position: relative;
+    color: #fff;
 }
 
 
+#banner .two {
+    background: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18459_f7b0a901-a6ef-47f0-bfac-3e98659eeca9.jpg?v=1679568839') center center/cover no-repeat;
+}
+
+#banner .two>div,
+#banner .one>div {
+    max-width: 1280px;
+    margin: auto;
+}
+
+#banner .two>div {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 10vh;
+}
+
+#banner .two img {
+    max-width: 408px;
+    max-height: 230px;
+}
+
+#banner .two .left {
+    transform: translateY(30px);
+}
+
+#banner .two .u24Medium {
+    margin-top: 26px;
+    margin-bottom: 46px;
+}
+
+#banner .two .my-button {
+    margin-right: 15px;
+}
 
 
-.store-list .list .u30DemiBold {
+#banner .one {
+    background: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18457_c4868f46-509a-4fc5-bdfd-5f36275dd375.jpg?v=1679568657') center bottom/cover no-repeat;
+}
+
+#banner .one > div {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+}
+
+#banner .one > div .right {
+    max-width: 577px;
+    display: flex;
+    flex-direction: column;
+}
+
+#banner .one > div .right .texts {
+    display: flex;
+    align-items: center;
+}
+
+#banner .one > div .right > img {
+    margin-top: 3vh;
+    margin-bottom: 6vh;
+}
+
+#banner .one > div .right .texts .u20Medium {
+    font-size: 18px;
+    line-height: 18px;
+    margin-right: 12px;
+    transform: translateY(-5px);
+}
+
+#banner .one > div .right .box {
+    display: flex;
+    align-items: center;
+}
+
+#banner .one > div .right .box > span {
+    width: 56px;
+    height: 56px;
+    display: block;
+    background-color: #fff;
+    border-radius: 50%;
+    color: #479ABA;
+    font-size: 50px;
+    text-align: center;
     line-height: 50px;
+    min-width: 56px;
+    margin-right: 25px;
 }
 
-.store-list .list ul {
-    margin: 6px 0 12px;
-    padding: 0;
-    padding-left: 20px;
+#banner .one > div .right .box > div {
+    background-color: #fff;
+    padding: 14px;
+    border-radius: 14px;
+    display: flex;
+    flex: 1;
+    max-width: 480px;
 }
 
-.store-list .list .u20Medium {
-    margin-bottom: 6px;
-    font-size: 17px;
-    color: #555;
+#banner .one > div .right .box > div .item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #000;
+    text-align: center;
+    flex: 1;
 }
 
-.store-list .list .address {
-    padding-top: 6px;
-    height: 60px;
+#banner .one > div .right .box > div .item img {
+    max-width: 72px;
 }
 
-.store-list .list ul>li::marker {
-    color: #FD4B17;
+#banner .one > div .right .box > div .item span {
+    font-size: 14px;
+    line-height: 18px;
 }
 
-.store-list .list .my-button {
-    background-color: #FD4B17;
-    padding: 10px 40px;
+#banner .one > div .right .box > div .item > div {
+    background-color: #479ABA;
+    border-radius: 20px;
+    color: #fff;
+    padding: 10px 20px;
+    margin-bottom: 12px;
+}
+
+#banner .one > div .right .buttons {
+    display: flex;
+    margin-top: 6vh;
+    justify-content: center;
+    width: 100%;
+}
+
+#banner .one > div .right .buttons .my-button {
+    margin: 0 10px;
+}
+
+#banner .swiper-pagination-bullet {
+    height: 6px;
+    width: 6px;
+    background-color: #d5d5d5;
+    opacity: 0.5;
+}
+
+#banner .swiper-pagination-bullet-active {
+    width: 30px;
+    height: 6px;
+    border-radius: 5px;
+    transition: all .3s;
+    opacity: 1;
 }
 
 
 @media (max-width: 767px) {
-    .store-list {
-        margin: -20px 0 auto;
-        /* grid-template-columns: 1fr; */
-        /* grid-template-rows: 1fr 3fr; */
-        display: flex !important;
+    #banner,
+    #banner .swiper-wrapper,
+    #banner .swiper-slide {
+        height: 746px;
+    }
+
+    #banner .two {
+        background: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18461.jpg?v=1679560804') center center/cover no-repeat;
+        position: relative;
+    }
+
+
+    #banner .two>div {
+        padding-top: 3.5vh;
         flex-direction: column;
-    }
-
-	.store-list .list {
-        order: 1;
-        padding: 0;
-        flex: 1;
-   }
-
-   .store-list .list .u30DemiBold {
-        font-size: 24px;
-        line-height: 30px;
-        margin-bottom: 12px;
-   }
-
-   .store-list .list .u20DemiBold,
-   .store-list .list .u20Medium {
-        font-size: 16px;
-        line-height: 20px;
-   }
-
-   .store-list .list .my-button {
-        width: 100%;
         text-align: center;
-   }
-
-   .store-list .list .address {
-     height: auto;
-   }
-
-   .store-list .list ul {
-      margin-bottom: 0;
-   }
-  }
-</style>
-
-
-<style>
-  #storepoint-tag-dropdown,
-  #storepoint-search,
-  #storepoint-panel {
-	 display:none!important;
-  }
-  
-  #storepoint-map,
-  #storepoint-container {
-	width: 100% !important;
-    height: 593px !important;
-  }
-
-  /* #storepoint-container .mapboxgl-ctrl-top-right {
-        display: none;
-  } */
-  
-  @media (max-width: 767px) {
-	#storepoint-container,
-    #storepoint-map {
-        max-height: 30vh !important;
-	  /* min-height: calc(100vw - 30px); */
-    }
-  }
-
-  /* @media (min-width: 768px) {
-    #storepoint-container {
-        pointer-events: none;
     }
 
-    #storepoint-container .stpt-marker {
-        pointer-events: all;
-    }    
-  } */
+
+    #banner .two .left {
+        transform: none;
+    }
+
+
+    #banner .two img {
+        max-width: 189px;
+        max-height: 107px;
+        margin: auto;
+    }
+
+    #banner .two h2 {
+        font-size: 36px;
+        line-height: 40px;
+    }
+
+    #banner .two .u24Medium {
+        margin-top: 10px;
+        margin-bottom: 27px;
+        font-size: 16px;
+        line-height: 22px;
+    }
+
+    #banner .buttons {
+        position: absolute;
+        bottom: 60px;
+        left: 0;
+        right: 0;
+        text-align: center;
+    }
+
+
+    #banner .one {
+        background: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18465_2x_8d57d433-07f3-408d-9cb9-932ca2943bf3.jpg?v=1679572207') center bottom/cover no-repeat;
+        padding: 10%;
+    }
+
+    #banner .one > div {
+        align-items: flex-start;
+    }
+
+    #banner .one > div .right {
+        align-items: center;
+        width: 100%;
+    }
+
+    #banner .one > div .right .texts .u48DemiBold {
+        font-size: 24px;
+        line-height: 20px;
+    }
+
+    #banner .one > div .right > img {
+        max-width: 224px;
+        margin-top: 10%;
+        margin-bottom: 10%;
+    }
+
+    #banner .one > div .right .box {
+        width: 100%;
+    }
+
+    #banner .one > div .right .box > div {
+        padding: 6px;
+    }
+
+    #banner .one > div .right .box > div .item img {
+        max-width: 45px;
+    }
+
+    #banner .one > div .right .box > div .item span {
+        font-size: 11px;
+        line-height: 13px;
+    }
+
+    #banner .one > div .right .box > div .item > div {
+        padding: 4px 14px;
+        font-size: 12px;
+    }
+}
 </style>
 
 <template>
-    <!-- <div class="hide">
-        <div class="store-list">
-            <div class="list">
-                <div class="item">
-                    <div class="u30DemiBold">ELV Bikes</div>
-                    <div class="u20DemiBold">Available Model:</div>
-                    <ul>
-                        <li class="u20Medium">Carbon One Size M/L</li>
-                        <li class="u20Medium">Chord</li>
-                    </ul>
-                    <div class="u20DemiBold">ADD:</div>
-                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
-                    <div class="my-button my-button-black">Select</div>
-                </div>
-                <div class="item">
-                    <div class="u30DemiBold">ELV Bikes</div>
-                    <div class="u20DemiBold">Available Model:</div>
-                    <ul>
-                        <li class="u20Medium">Carbon One Size M/L</li>
-                        <li class="u20Medium">Chord</li>
-                    </ul>
-                    <div class="u20DemiBold">ADD:</div>
-                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
-                    <div class="my-button my-button-black">Select</div>
+    <div id="banner">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide two">
+                <div>
+                    <div class="left">
+                        <h2>HUGE <br>EASTER SALE</h2>
+                        <div class="u24Medium">Up to 30% OFF</div>
+                        <div class="buttons">
+                            <a class="my-button" href="">Shop Now</a>
+                            <a class="my-button my-button-black" href="">Test Rides</a>
+                        </div>
+                    </div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18455.png?v=1679559152"
+                        alt="">
                 </div>
             </div>
-            <div id="storepoint-container" data-tags="de,Test Ride,Münster" data-map-id="162f21804990ff"></div>
+            <div class="swiper-slide one">
+                <div>
+                    <div class="right">
+                        <div class="texts">
+                            <span class="u20Medium pcHide">Huge Easter Sale</span>
+                            <span class="u48DemiBold">Carbon 1</span>
+                        </div>
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18487.png?v=1679567557" alt="">
+                        <div class="box">
+                            <span class="pcHide">+</span>
+                            <div>
+                                <div class="item">
+                                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18445.jpg?v=1679565530" alt="">
+                                    <span>Water Bottle <br>Cage</span>
+                                </div>
+                                <div class="item">
+                                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18444.jpg?v=1679565970" alt="">
+                                    <span>Micro <br>Fender</span>
+                                </div>
+                                <div class="item">
+                                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Image_140.jpg?v=1679566024" alt="">
+                                    <span>Kickstand</span>
+                                </div>
+                                <div class="item" style="max-width: 87px;">
+                                    <div>FREE</div>
+                                    <span style="text-decoration: line-through;">€173</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="buttons">
+                            <a class="my-button" href="">Shop Now</a>
+                            <!-- <a class="my-button my-button-black" href="">Test Rides</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div> -->
+        <div class="swiper-pagination"></div>
+    </div>
 </template>
+
 
 <script setup>
 import { onMounted } from 'vue';
+
+
+onMounted(() => {
+
+    new Swiper('#banner', {
+        // autoplay: {
+        //     disableOnInteraction: false,
+        //     pauseOnMouseEnter: true,
+        //     delay: 5000,
+        // },
+        // loop: true,
+        speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        }
+    })
+})
 
 
 </script>
---
+++ src/App19.vue
@@ -0,0 +1,206 @@
+<style>
+.store-list {
+    display: grid !important;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1%;
+    /* margin: -20px; */
+}
+
+.store-list .list {
+    max-height: 593px;
+    overflow: auto;
+    padding-right: 10%;
+}
+
+.store-list .list .item {
+    padding: 12px 0;
+}
+
+.store-list .list .item + .item {
+    border-top: 1px solid #ccc;
+}
+
+
+
+
+.store-list .list .u30DemiBold {
+    line-height: 50px;
+}
+
+.store-list .list ul {
+    margin: 6px 0 12px;
+    padding: 0;
+    padding-left: 20px;
+}
+
+.store-list .list .u20Medium {
+    margin-bottom: 6px;
+    font-size: 17px;
+    color: #555;
+}
+
+.store-list .list .address {
+    padding-top: 6px;
+    height: 60px;
+}
+
+.store-list .list ul>li::marker {
+    color: #FD4B17;
+}
+
+.store-list .list .my-button {
+    background-color: #FD4B17;
+    padding: 10px 40px;
+}
+
+
+@media (max-width: 767px) {
+    .store-list {
+        margin: -20px 0 auto;
+        /* grid-template-columns: 1fr; */
+        /* grid-template-rows: 1fr 3fr; */
+        display: flex !important;
+        flex-direction: column;
+    }
+
+	.store-list .list {
+        order: 1;
+        padding: 0;
+        flex: 1;
+   }
+
+   .store-list .list .u30DemiBold {
+        font-size: 24px;
+        line-height: 30px;
+        margin-bottom: 12px;
+   }
+
+   .store-list .list .u20DemiBold,
+   .store-list .list .u20Medium {
+        font-size: 16px;
+        line-height: 20px;
+   }
+
+   .store-list .list .my-button {
+        width: 100%;
+        text-align: center;
+   }
+
+   .store-list .list .address {
+     height: auto;
+   }
+
+   .store-list .list ul {
+      margin-bottom: 0;
+   }
+  }
+</style>
+
+
+<style>
+  #storepoint-tag-dropdown,
+  #storepoint-search,
+  #storepoint-panel {
+	 display:none!important;
+  }
+  
+  #storepoint-map,
+  #storepoint-container {
+	width: 100% !important;
+    height: 593px !important;
+  }
+
+  /* #storepoint-container .mapboxgl-ctrl-top-right {
+        display: none;
+  } */
+  
+  @media (max-width: 767px) {
+	#storepoint-container,
+    #storepoint-map {
+        max-height: 30vh !important;
+	  /* min-height: calc(100vw - 30px); */
+    }
+  }
+
+  /* @media (min-width: 768px) {
+    #storepoint-container {
+        pointer-events: none;
+    }
+
+    #storepoint-container .stpt-marker {
+        pointer-events: all;
+    }    
+  } */
+</style>
+
+<template>
+    <div class="hideE">
+        <div class="store-list">
+            <div class="list">
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+                <div class="item">
+                    <div class="u30DemiBold">ELV Bikes</div>
+                    <div class="u20DemiBold">Available Model:</div>
+                    <ul>
+                        <li class="u20Medium">Carbon One Size M/L</li>
+                        <li class="u20Medium">Chord</li>
+                    </ul>
+                    <div class="u20DemiBold">ADD:</div>
+                    <div class="u20Medium address">425 S. Broadway, Los Angeles, CA 90013</div>
+                    <div class="my-button my-button-black">Select</div>
+                </div>
+            </div>
+            <div id="storepoint-container" data-tags="de,Test Ride,Münster" data-map-id="162f21804990ff">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+</script>

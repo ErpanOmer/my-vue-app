4b29b0f21f3bd9809b0d9ecce2147ff5cd6397f0
ErpanOmer <354073823@qq.com>
2023-04-04 19:10:59 +0800

    更新demo

--- src/App.vue
+++ src/App.vue
@@ -1,154 +1,247 @@
-<style>
-.mini-dialog {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: #000000b3;
-    justify-content: center;
-    align-items: center;
-    z-index: 100000000;
-    overflow: hidden;
-    padding: 0 8vw;
-    display: none;
-    user-select: none;
-}
-
-.mini-dialog > div {
-    position: relative;
-    z-index: 100000001;
-    max-height: 85vh;
-    overflow: hidden;
-}
-
-.subscribe-container {
-    display: grid;
-    max-width: 976px;
-    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
-    border-radius: 15px;
-    background-color: #fff;
-}
-
-.subscribe-container .subscribe {
-    display: flex;
-    flex-direction: column;
-    padding: 12%;
-    justify-content: center;
-}
-
-.subscribe-container .subscribe img {
-    margin: 30px 0;
-    max-width: 265px;
-}
-
-.subscribe-container .subscribe .u17Light {
-    line-height: 24px;
-    margin-bottom: 86px;
-}
-
-.subscribe-container .subscribe .email {
-    box-sizing: border-box;
-    display: flex;
-    height: 47px;
-    position: relative;
-}
-
-.subscribe-container .subscribe .email input {
-    height: 100%;
-    width: 100%;
-    border: 1.5px solid #0000004D;
-    border-radius: 30px;
-    color: #000;
-    padding: 0 25px;
-    font-size: 14px;
-    position: relative;
-    z-index: 1;
-    overflow: hidden;
-    box-sizing: border-box;
-}
-
-.subscribe-container .subscribe .email .my-button {
-    position: absolute;
-    right: 0;
-    z-index: 2;
-    height: 100%;
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-}
-
-.subscribe-container .subscribe .email input::placeholder {
-    color: #aaa;
-}
-
-.subscribe-container .close {
-    position: absolute;
-    top: 5px;
-    right: 20px;
-    font-size: 40px;
-    color: #999;
-    cursor: pointer;
-    font-weight: 300;
-}
-
-@media (max-width: 767px) {
-    .subscribe-container {
-        grid-template-columns: 1fr;
-    }
-
-    .subscribe-container .subscribe {
-        padding: 8%;
-    }
-
-    .subscribe-container .subscribe .u20Medium {
-        font-size: 18px;
-        line-height: 21px;
-    }
-
-    .subscribe-container .subscribe img {
-        margin: 15px 0;
-        max-width: 205px;
-    }
-
-    .subscribe-container .subscribe .u17Light {
-        font-size: 15px;
-        line-height: 21px;
-        margin-bottom: 40px;
-    }
-
-    .subscribe-container .subscribe .email {
-        height: 40px;   
-    }
-
-    .subscribe-container .subscribe .email input {
-        font-size: 12px;
-        padding: 0 15px;
-    }
-
-    .subscribe-container .close {
-        top: -5px;
-        right: 10px;
-        color: #fff;
-    }
-}
+<style lang="scss">
+@import './styles/testride.scss';
 </style>
 
 <template>
-    <!-- <div class="mini-dialog">
-        <div class="container">
-            <img class="mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Urtopia_Photo_Sesh_9_of_14.jpg?v=1680242058" alt="">
-            <img class="pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Urtopia_Photo_Sesh_9_of_14_2x_57b3bdef-c4dc-4f7a-a72e-8fbb41b596b9.jpg?v=1680245456" alt="">
-            <div class="subscribe">
-                <span class="u20Medium">Huge Easter Sale</span>
-                <span class="u17Light">Subscribe for Easter exclusive coupon and Utopia stories</span>
-                <div class="email">
-                    <input placeholder="Enter email here" type="email">
-                    <div class="my-button my-button-black">Subscribe</div>
+    <div class="testride">
+        <div class="banner">
+            <div class="u48DemiBold_v2">Your first hand on Urtopia</div>
+            <div class="u17Medium_v2">Take your first step in to experience the stunning appearance and smooth riding. Book
+                a test ride today with our partners and ambassadors! Their extensive expertise and enthusiasm will help you
+                out with any questions in and out.</div>
+        </div>
+        <div class="map">
+            <div class="centeral-content">
+                <div class="u36DemiBold_v2">Choose your location</div>
+                <div id="storepoint-container" data-tags="de" data-map-id="162f21804990ff"></div>
+            </div>
+        </div>
+        <div class="explained">
+            <div class="centeral-content">
+                <div class="u36DemiBold_v2">Test ride <br>options explained</div>
+                <div class="item">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/IMG_0884_2x_8bbd4ebe-02ed-45b5-9d98-34d41a1c9a9f.jpg?v=1680586879"
+                        alt="">
+                    <div class="title">
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/shop_2x_21db8206-c4af-4fe9-98e5-a23ed770e4e8.png?v=1680587063"
+                            alt="">
+                        <span class="u20DemiBold_v2">Partner Store</span>
+                    </div>
+                    <div class="u17Light_v2">There you can view and test ride our e-bike models, get in-person advice from
+                        our e-bike experts.
+                        We even have in-stock models ready for immediate purchase.</div>
+                </div>
+                <div class="item">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221005-untitled-9056_2x_1a6940cb-42b6-4047-9c86-b70dda745f36.jpg?v=1680587516"
+                        alt="">
+                    <div class="title">
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/user_2x_1ee32fa1-d94b-4eda-be5e-deb33536a73b.png?v=1680587584"
+                            alt="">
+                        <span class="u20DemiBold_v2">Ambassadors</span>
+                    </div>
+                    <div class="u17Light_v2">Ambassadors are Urtopia representatives who are passionate about our e-bikes.
+                        While an ambassador may not have every model on hand they are there to meet you to show you the ins
+                        and outs of our bikes. They offer real insight into the benefits of being a daily e-bike user.</div>
                 </div>
             </div>
-            <span class="close" onclick="closeMiniDialog()">×</span>
         </div>
-    </div> -->
+        <div class="howto">
+            <div class="centeral-content">
+                <div class="u36DemiBold_v2">How to Test Ride</div>
+                <div class="items">
+                    <div class="item">
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/booking-2_2x_9223282e-c6ac-4785-87d7-f8fa9bdeb492.png?v=1680588590"
+                            alt="">
+                        <span class="u20DemiBold_v2">Book It</span>
+                        <span class="u17Light_v2">Choose a location and pick the date, you will receive an email to welcome
+                            and confirm.</span>
+                    </div>
+                    <div class="item">
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/users_2x_2becf589-29a4-4869-a3ec-a43f56f637f6.png?v=1680588585"
+                            alt="">
+                        <span class="u20DemiBold_v2">Meet us</span>
+                        <span class="u17Light_v2">Bring nothing other than your curiosity. Meet our Local experts in your
+                            city.</span>
+                    </div>
+                    <div class="item">
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/bike_2x_b310fe93-d3f0-4623-9ffd-91265fb9abf2.png?v=1680588485"
+                            alt="">
+                        <span class="u20DemiBold_v2">Ride it out</span>
+                        <span class="u17Light_v2">Scan the QR code at store to login. Unlock the bike and all the
+                            fun.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="whatto">
+            <div class="centeral-content">
+                <div class="u36DemiBold_v2">What to experience</div>
+                <div class="u17Light_v2">From the effortless power of the electric motor to the comfortable and stylish
+                    design, experience the thrill for yourself by booking a test ride today!</div>
+                <div class="videos">
+                    <div>
+                        <video muted playsinline autoplay loop
+                            src="https://download-video.akamaized.net/2/playback/f70b68d5-6749-440a-917c-5e6932e65dd3/263aaac7?__token__=st=1680590201~exp=1680604601~acl=%2F2%2Fplayback%2Ff70b68d5-6749-440a-917c-5e6932e65dd3%2F263aaac7%2A~hmac=8c991bf5870a91c639ebf3a12b584570d832dbfec1b75c5ecfbf329a422109d7&r=dXMtZWFzdDE%3D"></video>
+                    </div>
+                    <div>
+                        <span class="u20DemiBold_v2 mobileHide">Full Carbon Fiber <br> Weight only 15 kg</span>
+                        <span class="u20DemiBold_v2 pcHide">Full Carbon Fiber <br> Weight only 15 kg</span>
+                        <span class="u20DemiBold_v2">Try the advantage <br> of a lightweight e-bike</span>
+                        <video muted playsinline autoplay loop
+                            src="https://download-video.akamaized.net/2/playback/329248e9-5ee4-4f6c-ae5a-486762074393/020cf87e-35a4d77b?__token__=st=1680590567~exp=1680604967~acl=%2F2%2Fplayback%2F329248e9-5ee4-4f6c-ae5a-486762074393%2F020cf87e-35a4d77b%2A~hmac=2904622bf6f32419944509595ababfcb815d498ca7488dc2bb9bb65956537b26&r=dXMtZWFzdDE%3D"></video>
+                        <span class="u20DemiBold_v2">The riding experience <br> will not let you down</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="smart">
+            <div class="centeral-content">
+                <div>
+                    <span class="u20DemiBold_v2">Smart</span>
+                    <span class="u36DemiBold_v2">Elevate<br class="mobileHide"> Your Ride</span>
+                    <span class="u17Light_v2">Explore with a customized smart partner: talk, navigate, and never lose track
+                        of the bike.</span>
+                    <a class="my-button my-button-black">Book a free test ride</a>
+                </div>
+                <img class="pcHide"
+                    src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_20473_2x_ec77d911-5440-468e-a1c9-9b50497550e5.jpg?v=1680597546"
+                    alt="">
+                <div class="mobileHide">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_20834_2x_e92f1711-9e53-401b-a684-f9d6babe870b.png?v=1680595868"
+                        alt="">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div class="notin">
+            <div style="background-color: rgba(0, 0, 0, 0.65);flex: 1;">
+                <div class="centeral-content">
+                    <span class="u36DemiBold_v2">Not in your city?</span>
+                    <span class="u17Light_v2">We got you covered, contact us for more test ride opportunity, or just visit
+                        one of our partner store to test ride.</span>
+                    <div class="buttons">
+                        <a class="my-button" href="">Contact us</a>
+                        <a class="my-button" href="">Find a dealer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="faqs">
+            <div class="centeral-content">
+                <div class="faq">
+                    <div class="titlee">Test-ride FAQs</div>
+                    <div class="swiper swiper-no-swiping faq-list">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    const faqs = [
+        {
+            title: 'How to book a test ride?',
+            content: `We know how important it is for you to test out the bike before you purchase it, therefore we build a system that you can easily book a test ride or find a test ride spot that best suit you.<br><br> Navigate to the below pages:<br> Test Ride <br>Find a Store <br><br> In the test ride page, you start with choosing your city, we are doing our best try to expand the our test ride locations, then fill in your informations and select a time that suit you the best. That’s all you need to book a test ride. After that you will receive a confirmation email, and you might receive a call from us to collect more details for your best experience. <br><br>In the Find a Store page, you can find all the Test Ride spot by choosing “Test Ride” in Store Service, you don’t need to book while going to these spot, but we highly recommend you to give the store a call before you go.<br><br> If you can not find a spot that works for you, please contact us at support@newurtopia , we will try our best to organise you a test ride.`
+        },
+        {
+            title: 'What to expect when attending a test ride?',
+            content: `The step is fairly simple, all our test ride spot is trained to answer any of your questions and will guide you through the process, the general step is like list below:<br><br>
+                        <ul style="padding-inline-start: 5%;">
+                        <li>Meet our expert: Bring nothing other than your curiosity. Meet our experts in store.</li>
+                        <li>Test ride: Scan the QR code at store to login. Unlock the bike and all the fun.</li>
+                        <li>Receive a test ride report: A taste of our smart experience will be sent to your email after the ride.</li>
+                        </ul>`
+        }
+    ]
+
+    $('.faqs .swiper-slide').html(faqs.map(faq => `
+      <div class="faq-item">
+         <div class="faq-title">
+           <span>${faq.title}</span>
+           <i class="faq-content-plus"></i>
+         </div>
+         <div class="faq-content">${faq.content}</div>
+      </div>`).join(''))
+
+
+    setTimeout(() => {
+        $('.faq-title').click(function () {
+            $(this)
+                .siblings('.faq-content')
+                .slideToggle()
+                .parent()
+                .siblings()
+                .find('.faq-content')
+                .slideUp();
+            $(this).children('i').toggleClass('faq-content-plus');
+            $(this)
+                .parent()
+                .siblings()
+                .find('.faq-title')
+                .children('i')
+                .addClass('faq-content-plus');
+        });
+    }, 300)
+
+
+    const a = document.createElement("script");
+    a.type = "text/javascript";
+    // a.async = !0;
+    a.src = "https://cdn.storepoint.co/api/v1/js/162f21804990ff.js";
+    const b = document.getElementsByTagName("script")[0];
+    b.parentNode.insertBefore(a, b);
+
+    // setTimeout(() => {
+    //     document.querySelector('.storepoint-location').addEventListener('click', console.log)
+
+    // }, 1000)
+
+
+    // 选择需要观察变动的节点
+    const targetNode = document.getElementById('storepoint-container');
+
+    // 观察器的配置（需要观察什么变动）
+    const config = { childList: true };
+
+    // 当观察到变动时执行的回调函数
+    const callback = function (mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for (let mutation of mutationsList) {
+            if (mutation.previousSibling) {
+
+                const id = mutation.previousSibling.id
+
+                if (id === 'storepoint-panel') {
+                    setTimeout(() => {
+                        $('#storepoint-results-container .storepoint-location').on('click', e => {
+                            console.log('e', e)
+                        })
+                    }, 500)
+                }
+
+
+                console.log(id)
+
+            }
+        }
+    };
+
+    // 创建一个观察器实例并传入回调函数
+    const observer = new MutationObserver(callback);
+
+    // 以上述配置开始观察目标节点
+    observer.observe(targetNode, config);
+
+    // 之后，可停止观察
+    // observer.disconnect();
+})
+</script>

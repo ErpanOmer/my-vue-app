258f4cdd53ec2d91e56ba1e893299c6318ce2b33
354073823@qq.com <354073823@qq.com>
2023-08-12 16:59:27 +0800

    feature: 更新列表

---
+++ src/apps/App42.vue
@@ -0,0 +1,284 @@
+<style lang="scss">
+@import '../styles/carbon-ebike-v2.scss';
+</style>
+
+
+<template>
+    <div class="carbon-ebike-v2">
+        <div class="top-fixed mobileHide">
+            <div class="centeral-content">
+                <span class="u20DemiBold_v2">Urtopia Carbon 1/1s</span>
+                <span class="flex-1"></span>
+                <div>
+                    <span class="u17Light_v2"><span class="u20DemiBold_v2 emphasize"
+                            style="font-weight: 600;">$2,199.00</span> <span
+                            style="text-decoration: line-through;">$2,799.00</span></span>
+                    <span class="u14Light_v2">or as low as $91.6/Month</span>
+                </div>
+                <a href="/products/urtopia-carbon-e-bike" class="my-button my-button-origin">order/lease now</a>
+            </div>
+        </div>
+        <div class="banner">
+            <div class="centeral-content">
+                <div class="u60Medium_v2">Urtopia<br>Carbon 1/1s</div>
+                <div class="u36Light_extra_v2">
+                    <span>Smarter</span>
+                    <span>Lighter</span>
+                    <span>Easier</span>
+                </div>
+                <div class="buttons mobileHide">
+                    <a href="" class="my-button">Buy now</a>
+                    <a href="" class="my-button">Book a test ride</a>
+                </div>
+            </div>
+        </div>
+        <div class="design">
+            <div class="centeral-content">
+                <div>
+                    <div class="u48Medium_v2">Design is <br class="pcHide">the Soul</div>
+                    <div class="logos">
+                        <img style="max-width: 210px;"
+                            src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18793_2x_bccf20b5-51e1-4dbc-abbe-a7359094c98c.png?v=1690619934"
+                            alt="">
+                        <img style="max-width: 78px;"
+                            src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21573_2x_3ef5c6d7-993d-4c20-bd6b-92f9703bb289.png?v=1690619929"
+                            alt="">
+                    </div>
+                    <div class="u17Light_v2">Formerly a designer for BMW and Siemens, and inspired by the Möbius strip, Mr.
+                        Heller built a stunning frame shape that symbolizes sustain- ability and interconnectedness.</div>
+                </div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/IMG_4817_2x_91d4bb29-16c3-4e76-b5e1-b9e01ffd66d1.jpg?v=1690619924"
+                    alt="">
+            </div>
+        </div>
+        <div class="full-carbon">
+            <div class="centeral-content">
+                <div class="mobileHide"></div>
+                <div>
+                    <div class="u24Medium_v2">Full Carbon</div>
+                    <div class="u48Medium_v2">Only 15kg</div>
+                    <div class="u17Light_v2">Adopting high-end racing bike materials enables Urtopia to be 30% lighter than
+                        most peers. No breath holding needed carrying Urtopia in-and-out.</div>
+                    <a href="" class="my-button my-button-origin">Book a Free Test Ride</a>
+                </div>
+            </div>
+        </div>
+        <div class="all-hands gray-background-color">
+            <div class="centeral-content">
+                <div class="u24Medium_v2">All in Hands</div>
+                <div class="u48Medium_v2">Smart is the <br class="pcHide">new sexy</div>
+                <div class="box">
+                    <div>
+                        <div class="u30Medium_v2">Turn by Turn<br> Navigation</div>
+                        <div class="u17Light_v2">Formerly a designer for BMW and Siemens, and inspired by the Möbius strip,
+                            Mr. Heller built a stunning frame shape that symbolizes sustain- ability and interconnectedness.
+                        </div>
+                    </div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/smartbar_2x_7fb3ab31-9cd0-4651-bf40-2251da955b26.jpg?v=1690622819"
+                        alt="">
+                </div>
+                <div class="movement">
+                    <div>
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Screen_Shot_2023-07-26_at_3.37.41_PM_2x_ee5079cc-29d4-489f-bb3c-76439ffdae28.jpg?v=1690623586"
+                            alt="">
+                        <div class="u24Medium_v2">Movement Alarm</div>
+                        <div class="u17Light_v2">Once locked, the onboard alarm monitors movements and alerts you against
+                            unauthorized ones.</div>
+                    </div>
+                    <div>
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21596_2x_92e68a96-6881-4d4e-b05e-b3b29da20aa2.jpg?v=1690623911"
+                            alt="">
+                        <div class="u24Medium_v2">GPS Tracking and Fence</div>
+                        <div class="u17Light_v2">Aided by a constant 4G connection, GPS tracking keeps you informed of the
+                            e-bike’s position, even in the worst scenario of theft.</div>
+                        <img class="mobileHide" style="max-width: 60px;cursor: pointer;margin-top: 24px;"
+                            src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Component_65_14_2x_2cce20fa-311c-4052-a7e1-d36f098e4e10.png?v=1690624190"
+                            alt="">
+                        <img class="pcHide" style="max-width: 60px;order: 0;cursor: pointer;margin-bottom: 24px;"
+                        src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Component_65_14_2x_2cce20fa-311c-4052-a7e1-d36f098e4e10.png?v=1690624190"
+                        alt="">
+                    </div>
+                    <div>
+                        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/smartbar_2x_3ad6bbde-f148-4822-a2ec-503bf3949326.jpg?v=1690623928"
+                            alt="">
+                        <div class="u24Medium_v2">Fingerprint Unlock</div>
+                        <div class="u17Light_v2">Fingerprint unlock provide maximum convenience whenever you hop on the
+                            e-bike. No more hassle worrying about losing another key.</div>
+                    </div>
+                </div>
+                <div class="strava">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21598_2x_8413d465-6048-4ce0-8dd8-c440846aa733.jpg?v=1690624545"
+                        alt="">
+                    <div>
+                        <div class="u30Medium_v2">Integrated with <br>Apple Watch and Strava</div>
+                        <div class="icons">
+                            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20230721-134446.png?v=1689918329"
+                                alt="">
+                            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Path_67707_2x_806d4e68-ba1e-4ce8-9b92-3b936a3ec301.png?v=1689909699"
+                                alt="">
+                        </div>
+                        <div class="u17Light_v2">Urtopia E-Bike allow syncing Apple Watch for Heart Rate Monitoring on
+                            Smartbar in real time. Urtopia App also break data barriers, sync with Apple Health, and share
+                            to Strava in just one click!</div>
+                        <a href="" class="my-button my-button-origin">Watch Video</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="safe-fun">
+            <div class="centeral-content">
+                <div>
+                    <div class="u30Medium_v2">Safe and Fun on Road</div>
+                    <div class="u24Medium_v2">Brake and Projection light</div>
+                    <div class="u17Light_v2">Brake light will flash when you brake to notice incoming traffic behind you.
+                        Projection light let you be more visible at night, also it makes you shine in the darkness.</div>
+                </div>
+                <div>
+                    <div class="u30Medium_v2 mobileHide">&nbsp;</div>
+                    <div class="u24Medium_v2">StVZO Front Light</div>
+                    <div class="u17Light_v2">Compliant with German Road Traffic Licensing Regulations, the ambient-light
+                        auto-controllable headlight can also be switched on and off via your voice, a Smartbar button, or
+                        the app.</div>
+                </div>
+            </div>
+        </div>
+        <div class="anti-glare-display">
+            <div class="centeral-content">
+                <div>
+                    <div class="u24Medium_v2">Anti-glare Display</div>
+                    <div class="u36Medium_v2">Big and Clear</div>
+                    <div class="u17Light_v2">Key information shown large and clear, keep your focus on the road.</div>
+                </div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Screen_Shot_2023-07-27_at_4.08.01_PM_2x_a98e9d04-337f-40ac-828c-bcdfba030c3d.jpg?v=1690769455"
+                    alt="">
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/2x.jpg?v=1690769757" alt="xxx">
+                <div>
+                    <div class="u36Medium_v2">Simple and Intuitive</div>
+                    <div class="u24Medium_v2">
+                        <div>Game pad Control</div>
+                        <div class="u17Light_v2">Know gaming, know riding. Take full control of your ride.</div>
+                    </div>
+                    <div class="u24Medium_v2">
+                        <div>Talk to it just like to your friend</div>
+                        <div class="u17Light_v2">Voice, the only command you’ll never forget, and a more intuitive and safe
+                            way to interact.</div>
+                    </div>
+                    <div class="u24Medium_v2">
+                        <div>Bluetooth Speaker</div>
+                        <div class="u17Light_v2">We know the importance of music, so we built in a bluetooth speaker, you
+                            can enjoy your favourite music when riding without carrying an external speaker.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="your-whole">
+            <div class="centeral-content">
+                <div class="u30Medium_v2">Your whole <br>journey in one <br class="mobileHide">app</div>
+                <div class="u17Light_v2">The app is an extension of your bike, <br class="mobileHide">allowing you to control and know
+                    everything <br class="mobileHide">there is.</div>
+            </div>
+        </div>
+        <div class="boxs gray-background-color">
+            <div class="centeral-content">
+                <div>
+                    <div class="u36Medium_v2">Track Your Rides and Fitness</div>
+                    <div class="u17Light_v2">Journey Captured, Memories Made. Routes, speed, calories burned…all captured.
+                    </div>
+                </div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21600_2x_f9bc180c-9dbc-4e6a-9eb0-5ac8665dcde6.jpg?v=1690771444"
+                    alt="">
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21601_2x_b25bd6fb-5eb6-48bc-af13-dd528dd3a81a.jpg?v=1690771453"
+                    alt="xxx">
+                <div>
+                    <div class="u36Medium_v2">Share Your Story and Meet New Friends</div>
+                    <div class="u17Light_v2">Share rides, photos, stories with Urtopia community in app.</div>
+                </div>
+            </div>
+        </div>
+        <div class="powertrain gray-background-color">
+            <div class="centeral-content">
+                <div class="u36Medium_v2">Powertrain</div>
+                <div class="u54Medium_v2">Invisible is Invincible</div>
+                <div class="box">
+                    <div>
+                        <div class="u30Medium_v2">Gearing</div>
+                        <div class="u24Medium_v2">
+                            <div>Carbon 1 - Carbon Belt</div>
+                            <div class="u17Light_v2">30,000-km maintenance free. Farewell to old chains and greasy hands.
+                            </div>
+                        </div>
+                        <div class="u24Medium_v2">
+                            <div>Carbon 1s - <br class="pcHide">Shimano 7-Speed</div>
+                            <div class="u17Light_v2">Provides a gear ratio range from 1.5 to 4, It means that you get up to
+                                40% more efficiency when riding up slopes, and up to 60% faster speed when pedaling at the
+                                same cadence.</div>
+                        </div>
+                    </div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/faf_2x_f6d24ae0-30b3-4bfc-8645-8886554a7aee.jpg?v=1690773780"
+                        alt="">
+                </div>
+            </div>
+        </div>
+        <div class="torque-sensor gray-background-color">
+            <div class="centeral-content">
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/2x_38ec2540-2e11-4aee-b202-e71b4637bd81.jpg?v=1690774576"
+                        alt="">
+                    <div class="u24Medium_v2">Torque sensor</div>
+                    <div class="u17Light_v2">Responsive and intuitive, we take smooth riding to the next level.</div>
+                </div>
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/2x_18a24f44-6c7d-414c-9c19-8f577636aa72.jpg?v=1690774584"
+                        alt="">
+                    <div class="u24Medium_v2">100-km Range Battery</div>
+                    <div class="u17Light_v2">9.8Ah Samsung Li-ion battery embedded in the down frame, removable and
+                        extendable.</div>
+                </div>
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Screen_Shot_2023-07-26_at_6.17.24_PM_2x_f4917c24-44fe-4da5-b0c1-98e69f34c0f2.jpg?v=1690774593"
+                        alt="">
+                    <div class="u24Medium_v2">5 Pedal Modes</div>
+                    <div class="u17Light_v2">A 250-W compact motor enables riding in comfort and cruising in peace.</div>
+                </div>
+            </div>
+        </div>
+        <div class="styles4">
+            <div class="centeral-content gray-background-color">
+                <div class="texts">
+                    <div class="u30Medium_v2">4 Styles</div>
+                    <div class="u17Light_v2">Always one that can fit your style.</div>
+                    <div class="my-button my-button-black">Choose now</div>
+                </div>
+                <div class="imgaes">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20220617-143005_2x_d8fe4f9a-0caf-4684-9c1d-a35fcad47d71.png?v=1690775625"
+                        alt="">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20220617-143000_2x_e34d2104-c5f6-4325-98d0-35b9c362d067.png?v=1690776151"
+                        alt="">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_18705_2x_f4fcd89d-d08f-4f46-a229-d8f9342731bf.png?v=1690776158"
+                        alt="">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18299_2x_42e548af-3d0b-4402-a481-208f3a42e104.png?v=1690776143"
+                        alt="">
+                </div>
+            </div>
+        </div>
+        <div class="accessories">
+            <div class="centeral-content">
+                <div>
+                    <div class="u30Medium_v2">Accessories</div>
+                    <div class="u17Light_v2">Multiple accessories suit your needs <br>and empower your riding.</div>
+                    <div class="my-button my-button-black">Choose now</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-fixed pcHide">
+        <a href="/products/urtopia-carbon-e-bike" class="my-button my-button-origin">order/lease now</a>
+        <div>
+            <span>
+                <span class="u17DemiBold_v2 emphasize">$2,199.00</span> 
+                <span class="u11Light_v2" style="text-decoration: line-through;">$2,799.00</span>
+            </span>
+            <span class="u11Light_v2">or as low as $91.6/Month</span>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App40.vue'
+import App from './apps/App42.vue'
 
 createApp(App).mount('#app')

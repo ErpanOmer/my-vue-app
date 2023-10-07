5f6c038385e8b447e59d288ffbd9d7aee05f490f
354073823@qq.com <354073823@qq.com>
2023-10-07 14:14:11 +0800

    合并代码

---
+++ src/apps/App44.vue
@@ -0,0 +1,134 @@
+<style lang="scss">
+@import '../styles/outlet_v2.scss';
+</style>
+
+<template>
+    <div class="banner">
+        <div class="centeral-content">
+            <div class="u48DemiBold_v2">The Best Lightweight E-Bikes</div>
+            <div class="u24Medium_v2">An all-in-one e-bike with a lightweight frame, GPS, navigation, and a speaker for the ultimate ride. And here’s the kicker – nobody can even tell you’re on an electric bike.</div>
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less">Less effort. More enjoyment.</div>
+    <div class="anywhere">
+        <div class="centeral-content">
+            <div>
+                <div class="u42DemiBold_v2">Go anywhere with ease.</div>
+                <div class="u17Light_v2">With an extendable range, featherlight frame, and sturdy components, the Carbon
+                    1/1s is engineered to get you through the workweek and set you free on weekend adventures.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+            </div>
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Urtopia_-_Auto_Rack_-_Original_Size_-_RAW-42_2x_b1672e80-9190-4131-8223-541297bcff33.jpg?v=1695723221&width=1200" alt="">
+        </div>
+    </div>
+    <div class="anywhere easy">
+        <div class="centeral-content">
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Artboard_1_copy_8_2x_114c8342-a134-42e6-bdc1-d88b8c846698.jpg?v=1695733941&width=1200" alt="">
+            <div>
+                <div class="u42DemiBold_v2">Easy on. Easy off.</div>
+                <div class="u17Light_v2">Experience convenience wherever you roll with easy on-and-off access and powerful assistance. Its comfort-focused frame and upright riding position ensures every ride with high enjoyment.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+            </div>
+        </div>
+    </div>
+    <div class="anywhere">
+        <div class="centeral-content">
+            <div>
+                <div class="u42DemiBold_v2">Urban legend.</div>
+                <div class="u17Light_v2">A thoroughly modern take on a classic design. Cruising with an upright riding position, lightweight feel, reflective accents and elegant style like a urban legend even when fully equipped.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+            </div>
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20230213-untitled-6611_2x_13765d4b-8c5d-4d82-ba62-672f4c856d3f.jpg?v=1695724945&width=1200" alt="">
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less">No one knows it’s electric</div>
+    <div class="centeral-content concealed">
+        <div>
+            <span class="u42DemiBold_v2">Well-concealed Battery</span>
+            <span class="u17Light_v2">Urtopia e-bikes achieve a clean look by skillfully concealing the battery, effectively camouflaging its e-bike identity.</span>
+            <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+        </div>
+        <img style="background-color: rgb(237, 237, 237)" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/3_1_2x_2c484cdd-e8c9-4529-a045-9a207ce9ece3.png?v=1695725167&width=800" alt="">
+        <img style="background-color: rgb(237, 237, 237)" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/2_2x_9a47dfea-6f17-4f69-9f92-1277f9a92ab6.png?v=1695725152&width=800" alt="">
+        <div>
+            <span class="u42DemiBold_v2">Invisible power</span>
+            <span class="u17Light_v2">The almost imperceptible yet powerful hub drive system ensures minimal effort and maximum fun.</span>
+            <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less">Forward-thinking features</div>
+    <div class="centeral-content concealed">
+        <div>
+            <span class="u42DemiBold_v2">Navigation without Cellphone</span>
+            <span class="u17Light_v2">Bright screen shows navigation clearly under daylight, no more ugly phone holder. <br><br>Let you explore the unknown safe and free.</span>
+            <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/smartbar_2x_b38dcae0-834d-4e8e-a958-7bf357520494.jpg?v=1695729585&width=800" alt="">
+        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_21596_2x_a758c572-9eda-4bee-bfac-82d0f713860d.jpg?v=1695729559&width=800" alt="">
+        <div>
+            <span class="u42DemiBold_v2">GPS Tracking and Fence</span>
+            <span class="u17Light_v2">Aided by a constant 4G connection, GPS tracking keeps you informed of the e-bike’s position, even in the worst scenario of theft.</span>
+            <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less">Your ride. Your way.</div>
+    <div class="centeral-content bikes">
+        <div>
+            <div class="u36DemiBold_v2">Carbon 1/1s</div>
+            <div class="u17Light_v2">Luxury, lightweight, lively. A premium carbon e-bike that’s effortlessly easy to lift and maneuver.</div>
+            <div class="flex-1"></div>
+            <img class="mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18857_2x_188312b5-5225-49b9-a074-d33edc7b209b.png?v=1695731101" alt="">
+            <img class="pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18860_2x_afe71302-e8ce-4654-8130-aaff94f9463e.png?v=1695780284" alt="">
+            <a href="" class="my-button my-button-origin">Buy now</a>
+        </div>
+        <div>
+            <div class="u36DemiBold_v2">Chord X</div>
+            <div class="u17Light_v2">Casual, capable, comfortable. Superior comfort, easy on-and-off all with a confident, upright riding position.</div>
+            <div class="flex-1"></div>
+            <img class="mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18858_2x_a4ce8c3f-d3e6-4fd5-b186-d3188cdf3d16.png?v=1695732269" alt="">
+            <img class="pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18861_2x_7772f125-3161-470a-8d20-44413d1dc7b5.png?v=1695780292" alt="">
+            <a href="" class="my-button my-button-origin">Buy now</a>
+        </div>
+        <div>
+            <div class="u36DemiBold_v2">Chord</div>
+
+            <div class="u17Light_v2">Classic, clean, coherent. Inspired by piano. Driven by Innovation. Enjoy the fusion of aesthetics and tech.</div>
+            <div class="flex-1"></div>
+            <img class="mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18859_2x_a916a476-e3b6-4612-9076-a9c429145ff4.png?v=1695732289" alt="">
+            <img class="pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18860_2x_afe71302-e8ce-4654-8130-aaff94f9463e.png?v=1695780284" alt="">
+            <a href="" class="my-button my-button-origin">Buy now</a>
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less"><span class="mobileHide">&nbsp;</span></div>
+    <div class="anywhere easy">
+        <div class="centeral-content">
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/R6P_5749_2x_0c133a2d-0a30-424f-a7c6-fbcf10a7a67c.jpg?v=1695733950&width=1200" alt="">
+            <div>
+                <div class="u42DemiBold_v2">Leasing Partners at Your Choice</div>
+                <div class="u17Light_v2">We offer you the opportunity to get your dream bike at low monthly leasing rates through various leasing providers.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Buy now</a>
+            </div>
+        </div>
+    </div>
+    <div class="anywhere">
+        <div class="centeral-content">
+            <div>
+                <div class="u42DemiBold_v2">Outlet E-Bikes</div>
+                <div class="u17Light_v2">Enjoy the Urtopia Outlet E-Bikes with exceptional deals. Act fast on limited outlet stock featuring the same Urtopia quality you trust, complete with our guarantee and direct service.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Lease Now</a>
+            </div>
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20230926-092553_2x_75a99e6a-a145-446a-b206-9ad7d1796fcc.jpg?v=1695733957&width=1200" alt="">
+        </div>
+    </div>
+    <div class="anywhere easy">
+        <div class="centeral-content">
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/0W0A7526_2x_cf8fc1a6-ed6b-4521-886a-37721c7b5d66.jpg?v=1695733965&width=1200" alt="">
+            <div>
+                <div class="u42DemiBold_v2">Our Services and Testride Near You</div>
+                <div class="u17Light_v2">We craft bikes for riders worldwide. Locate a trusted Urtopia retailer nearby for test rides, questions, service, and a stronger connection to your local riding community.</div>
+                <a href="/products/urtopia-carbon-e-bike" target="_blank" class="my-button my-button-origin">Book a Free Test Ride</a>
+            </div>
+        </div>
+    </div>
+    <div class="centeral-content u48DemiBold_v2 less"><span class="mobileHide">&nbsp;</span></div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App43.vue'
+import App from './apps/App44.vue'
 
 createApp(App).mount('#app')

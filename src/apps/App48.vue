8dab614b7a58bc57ea93ca3505956e101b4dd392
ErpanOmer <354073823@qq.com>
2023-11-20 09:47:35 +0800

    merge: 合并代码

---
+++ src/apps/App48.vue
@@ -0,0 +1,147 @@
+<style lang="scss">
+@import '../styles/chord.scss';
+</style>
+
+<template>
+    <div class="maksim-big-banner">
+        <div class="centeral-content">
+            <div>
+                <div class="u36Medium_v2">Design Inspired <br>from Piano</div>
+                <div class="u17Light_v2">Chord, like a beautiful piano piece, bringing unprecedented smoothness and passion
+                    to the realm of bike, has won broad applause such as the German Design Award.</div>
+                <img src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20231114-233206_2x_b057955c-2082-4a23-a9ea-daed62b2f13e.png?v=1700012494"
+                    alt="">
+            </div>
+            <div>
+                <div class="u24Medium_v2">Maksim Mrvica</div>
+                <div class="u17Light_v2">World’s fastest pianist from Croatia, who blends classical grace with vibrant
+                    contemporary style. As the concept designer, he extends his aesthetic to the Chord, where elegance and
+                    comfort interwine seamlessly.</div>
+                <div class="my-button my-button-ghost">Watch video</div>
+            </div>
+        </div>
+    </div>
+    <div class="gray-background-color swiperrrrr">
+        <div class="centeral-content">
+            <div>
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" style="--bg-image: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23217_2x_fad69088-980d-4d97-be93-f78d1f4b74ed.jpg?v=1699950381');--bg-image-mb: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/chord_2x_8d52dbc8-959d-4da6-ab38-a067d7b0d171.jpg?v=1699960654');"></div>
+                    <div class="swiper-slide" style="--bg-image: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23216_2x_eb135a16-9a6a-4835-9cb5-171cb27e6d83.jpg?v=1699950385');--bg-image-mb: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/chord_2x_cd81b463-1d38-48bc-abcf-7ad667556fa8.jpg?v=1699960659');"></div>
+                    <div class="swiper-slide" style="--bg-image: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23215_2x_a626490b-314f-4f01-92bd-be79160fb110.jpg?v=1699950391');--bg-image-mb: url('https://cdn.shopify.com/s/files/1/0633/2068/6808/files/chord_2x_9e9ca0c8-2127-4d35-b86f-9584415ad074.jpg?v=1699960665');"></div>
+                </div>
+            </div>
+            <div class="swiper-button-prev mobileHide"></div>
+            <div class="swiper-button-next mobileHide"></div>
+            <div class="swiper-pagination pcHide"></div>
+            <div class="texts">
+                <div class="u24Medium_v2">Artisanal Grace from <br>Superior Craftsmanship</div>
+                <div class="u17Light_v2 active">
+                    <span></span>
+                    <span>No weld scar, Liquid Forging technology makes a seamless frame with perfect curve</span>
+                </div>
+                <div class="u17Light_v2">
+                    <span></span>
+                    <span>Water-based Multi-layer Paint shines quietly and touches as exquisite as a piano</span>
+                </div>
+                <div class="u17Light_v2">
+                    <span></span>
+                    <span>Removable Battery on the top tube completes the entire design like a piano key</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="gray-background-color boxs3645">
+        <div class="centeral-content">
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18918_2x_1f5b481d-45bf-4f00-bdbf-860723102593.jpg?v=1699942828"
+                alt="">
+            <div>
+                <div class="u24Medium_v2">A Comfortable Cruiser <br>to Levitate Your Ride</div>
+                <div class="u17Light_v2">The upright riding position, with ergonomic handlebar and adjustable stem, ensures
+                    ultimate comfort for the rider. The proprietary sensing and driving system gives intuitive power
+                    assistance and makes every pedal as light as wind.</div>
+            </div>
+            <div>
+                <div class="u24Medium_v2">A Considerate Mate <br>to Meet Your Need</div>
+                <div class="u17Light_v2">With a superb 120km range, you can simply forget about battery anxiety. Abundant
+                    accessories and reliable gears make the bike always practical, whether you are cruising in old towns,
+                    navigating in modern cities or travelling through countryside. Chord is a perfect mate for a cozy and
+                    loose lifestyle.</div>
+            </div>
+            <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/DSCF1992_2x_470c2e19-f48a-4f2c-af82-ec35889cd5f2.jpg?v=1699942729"
+                alt="">
+        </div>
+    </div>
+    <div class="gray-background-color easy">
+        <div></div>
+        <div>
+            <div class="u36Medium_v2">Easy life , Smart fun</div>
+            <div class="u17Light_v2">
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18337_2x_9eb33291-ec3e-4492-ba99-2562cc1eccae.png?v=1699944156"
+                    alt="">
+                <span>Finger-print unlock and GPS anti-theft provide unparalleled security for a carefree ride </span>
+            </div>
+            <div class="u17Light_v2">
+                <img src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20231115-093430.png?v=1700012088"
+                    alt="">
+                <span>Daylight-bright screen and built-in navigation show routes clearly in all weather conditions. Put your
+                    cell phone in your pocket and never worry about accidental drop or battery drain.</span>
+            </div>
+        </div>
+    </div>
+    <div class="yourself">
+        <div class="centeral-content">
+            <div>
+                <div class="u36Medium_v2">Be Yourself, Have Fun</div>
+                <div class="u17Light_v2">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18340_2x_c05ffbf0-9c05-4ef6-a375-ce54c68069df.png?v=1699946828"
+                        alt="">
+                    <span>Smartbar plays your music like a musicbox. Let your rhythm resonate with Chord</span>
+                </div>
+                <div class="u17Light_v2">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Mask_Group_18342_2x_5ccf7f37-a1b9-4fe7-949e-52c71f08b44c.png?v=1699946832"
+                        alt="">
+                    <span>Smart App tracks your journey and health. Get on the bike and share your thrilling moments with
+                        your friends and family.</span>
+                </div>
+                <img src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20231115-093417.png?v=1700012084"
+                    alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+    const swiperrrrr = new Swiper('.swiperrrrr .centeral-content > div:first-child', {
+        loop: true,
+        speed: 1000,
+        // direction: 'vertical',
+        navigation: {
+            nextEl: '.swiperrrrr .centeral-content .swiper-button-next',
+            prevEl: '.swiperrrrr .centeral-content .swiper-button-prev',
+        },
+        on: {
+            slideChange: function () {
+                $('.swiperrrrr .texts .u17Light_v2').removeClass('active')
+                $($('.swiperrrrr .texts .u17Light_v2')[this.realIndex]).addClass('active')
+            },
+        },
+        pagination: {
+           el:'.swiper-pagination'
+         },
+    })
+
+
+    $('.swiperrrrr .texts .u17Light_v2').on('click', function (e) {
+        $(this).closest('.texts').find('.u17Light_v2').removeClass('active')
+        $(this).addClass('active')
+
+        // console.log('index', )
+
+        swiperrrrr.slideTo($(this).index())
+    })
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App47.vue'
+import App from './apps/App48.vue'
 
 createApp(App).mount('#app')

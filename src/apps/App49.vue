4401f3ae5c578369842d77aa77a381b8a481710e
ErpanOmer <354073823@qq.com>
2023-12-05 09:36:55 +0800

    merge: 合并代码

---
+++ src/apps/App49.vue
@@ -0,0 +1,117 @@
+<style lang="scss">
+@import '../styles/white-glow.scss';
+</style>
+
+<template>
+    <div class="banner" style="--swiper-pagination-bullet-horizontal-gap: 10px;">
+        <div class="u24Medium_v2 mobileHide">White Glow</div>
+        <div class="u60Medium_v2">Christmas Special Edition</div>
+        <div class="images">
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23223_2x_65243ad2-a45b-40f3-afb3-0593518fc137.png?v=1700450177"
+                    alt="">
+                <div class="u17Medium_v2">Complete set of <br>free accessories</div>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23219_2x_283c3d24-1e6c-43da-ba74-a62ce2405718.png?v=1700449947"
+                    alt="">
+                <div class="u17Medium_v2">Limited Edition <br>20 Units Worldwide</div>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23224_2x_ef74bca7-ca37-4a0f-acba-ee7c55fcdf94.png?v=1700449941"
+                    alt="">
+                <div class="u17Medium_v2">Exclusive 1v1 <br>customer service</div>
+            </div>
+        </div>
+        <div class="swiperrrrr">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/MaskGroup18951.png?v=1700445339"
+                        alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/MaskGroup18954.png?v=1700445339"
+                        alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/MaskGroup18952.png?v=1700445339"
+                        alt="">
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="bottommmm mobileHide">
+            <img class="swiper-button-prev"
+                src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20231120-133606.png?v=1700458586" alt="">
+            <img class="swiper-button-next"
+                src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20231120-133606.png?v=1700458586" alt="">
+        </div>
+    </div>
+    <div class="gray-background-color product">
+        <div class="centeral-content">
+            <div>
+                <div class="u30DemiBold_v2">White Glow <span class="u20Medium_v2" style="color: #C6241F;">(Christmas Special)</span></div>
+                <div class="u20Light_v2"><span style="font-weight: 500;">5 left |</span> total 20 units</div>
+                <div class="u17Light_v2">For this Christmas, Urtopia is releasing a limited edition paint job. Crafting the warmest white from 150 shades of white, making this ‘White Glow’ a radiant warmth in your winter days. Limited edition, only 20 available for sale.”</div>
+                <div class="u17Medium_v2" style="text-decoration: underline;">Free Shipping within 5 days</div>
+            </div>
+            <div>
+                <div class="u17Light_v2"><span class="u17DemiBold_v2">Size -</span> fit for 175-195 cm</div>
+                <div class="u17Light_v2"><span class="u17DemiBold_v2">Color -</span> White Glow</div>
+                <div class="u17DemiBold_v2">Free Accessories</div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23292_2x_03443e75-74d6-4b8c-9814-d8b69a3b640b.jpg?v=1700469996" alt="">
+            </div>
+            <div></div>
+        </div>
+    </div>
+    <div class="centeral-content inspired">
+        <div class="u72Medium_v2">Inspired by <br>The Powder Snow in Sunshiny</div>
+        <img class="mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23288_2x_a0b994af-e5af-4761-be23-6dae0fff4eb0.jpg?v=1700465551" alt="">
+        <img class="pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23234_2x_354116db-1840-4e6e-a589-52751babf501.jpg?v=1700465999" alt="">
+        <div class="u17Light_v2">Sunlight reflects on the first snow, casting a warm white glow. Embraced by the festive Christmas vibes, White Glow’s design features this concept. The frame is adorned with rainbow lines, adding a touch of Christmas color to its silhouette.</div>
+    </div>
+    <div class="art">
+        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23289_2x_724b8230-013a-4df1-b9c7-4fea5a977837.jpg?v=1700466281" alt="">
+        <div>
+            <div class="u48Medium_v2">The Art of Handcrafted White</div>
+            <div class="u17Light_v2">This is a color you won’t find elsewhere, meticulously crafted through continuous experimentation among over 150 shades of white. It’s a completely handmade color, and due to its immense complexity in production, only 20 units are available in limited edition.</div>
+        </div>
+    </div>
+    <div class="centeral-content inspired">
+        <div class="u72Medium_v2">A Gift, <br class="mobileHide">for You or Your Lover</div>
+        <img class="mobileHide" style="margin-bottom: 50px;" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221008-untitled-4661_copy_2x_1d83ff0a-ada4-4126-949e-7a24ae841b85.jpg?v=1700467088" alt="">
+        <img class="pcHide" style="margin-bottom: 10vw;" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221008-untitled-4661_copy_2x_741d3123-5b68-46aa-9be9-4c4c7c76879d.jpg?v=1700467261" alt="">
+        <div class="u17Light_v2">Create lasting memories this Christmas—gift your loved one a limited edition white glow. Ride together and become a special sight on the streets. You’re not just 1 out of 20; you’re each other’s unique world.</div>
+    </div>
+    <div class="centeral-content vip">
+        <div>
+            <div class="u48Medium_v2">You are VIP</div>
+            <div class="u17Light_v2">For our exclusive limited edition customers, enjoy a tailored experience and get a complete set of accessories. Once you make a purchase, our dedicated customer service will reach out promptly, prioritizing your every question.</div>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23291_2x_5511c539-554d-453f-ba0f-e26038d66617.jpg?v=1700467836" alt="">
+    </div>
+    <img class="bottom-image mobileHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221010-untitled-7853_2x_1c9abfb6-c387-4da0-9a0b-3cde11959100.jpg?v=1700468729" alt="">
+    <img class="bottom-image pcHide" src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221010-untitled-7853_2x_cc6ddf36-8722-4cfd-977c-ee16acec099b.jpg?v=1700468899" alt="">
+</template>
+
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+    new Swiper('.banner .swiperrrrr', {
+        loop: true,
+        speed: 1000,
+        // direction: 'vertical',
+        navigation: {
+            nextEl: '.banner .bottommmm .swiper-button-next',
+            prevEl: '.banner .bottommmm .swiper-button-prev',
+        },
+        pagination: {
+            el: '.banner .swiper-pagination',
+            clickable: true
+        },
+    })
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App48.vue'
+import App from './apps/App49.vue'
 
 createApp(App).mount('#app')

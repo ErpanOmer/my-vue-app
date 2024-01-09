a7e7eb0ca415e9613688cbd3c998ad787b9dc718
ErpanOmer <354073823@qq.com>
2024-01-09 09:46:27 +0800

    update: 更新vue

---
+++ src/apps/App54.vue
@@ -0,0 +1,116 @@
+<style lang="scss">
+@import '../styles/home_v2.scss';
+</style>
+
+<template>
+    <div class="banner">
+        <h1 class="u60Medium_v2">Carbon 1 Pro</h1>
+        <h3 class="u20Medium_v2">Sporty and Lightweight</h3>
+        <a class="my-button my-button-ghost" href="">Learn More</a>
+    </div>
+    <div class="banner">
+        <h1 class="u60Medium_v2">Chord</h1>
+        <h3 class="u20Medium_v2">Comfort and Elegance</h3>
+        <a class="my-button my-button-white" href="">Learn More</a>
+    </div>
+    <div class="mind">
+        <h3 class="u36Medium_v2">The Bike with a Mind</h3>
+        <p class="u17Light_v2">
+            <span>
+                An AI-Driven Smartebike, offering you more than riding.
+            </span>
+        </p>
+        <a class="my-button my-button-white" href="">Learn More</a>
+    </div>
+    <div class="smart">
+        <div>
+            <h3 class="u36Medium_v2">Smart App</h3>
+            <p class="u17Light_v2">
+                <span>
+                    Connect, track, share, achieve - Urtopia app: Your perfect cycling companion!
+                </span>
+            </p>
+        </div>
+        <div>
+            <h3 class="u36Medium_v2">Sustainability</h3>
+            <p class="u17Light_v2">
+                <span>
+                    Sustainability at the Heart of Urtopia
+                </span>
+            </p>
+            <a class="my-button my-button-white" href="">Learn More</a>
+        </div>
+    </div>
+    <div class="test-ride">
+        <div class="centeral-content">
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Screenshot_2024-01-03_at_5.18.46_PM_2x_d12d7e8b-a5a6-4e2a-af50-54934fc52cee.jpg?v=1704525928"
+                    alt="" class="mobileHide">
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Screenshot_2024-01-03_at_5.18.46_PM_2x_39df59a9-0e10-440f-b9e4-032438fc14ef.jpg?v=1704527270"  class="pcHide" alt="">
+                <h2 class="u36Medium_v2">
+                    Test Ride Spots
+                </h2>
+                <div class="flex-1"></div>
+                <p class="u17Light_v2">
+                    <span>
+                        Experience the bike in real
+                    </span>
+                </p>
+                <a href="">Learn more</a>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20230520-untitled-8826_2x_7e43ad38-e398-4728-9ad2-d3ae2d91333d.jpg?v=1704525890"
+                    alt="" class="mobileHide">
+                    <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20230520-untitled-8826_2x_f8ac700e-c86e-4d2f-b37d-177624cf29a9.jpg?v=1704527262" class="pcHide" alt="">
+                <h2 class="u36Medium_v2">
+                    Peace of Mind
+                </h2>
+                <div class="flex-1"></div>
+                <p class="u17Light_v2">
+                    <span>
+                        24/7 Support, Full After-Sales
+                    </span>
+                </p>
+                <a href="">Learn more</a>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221005-untitled-8655_2x_45235a17-d68a-415e-9b0d-37bd9c86086b.jpg?v=1704525896"
+                    alt="" class="mobileHide">
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/20221005-untitled-8655_2x_27950ad5-b865-40ee-aced-4e65f9942de7.jpg?v=1704527258" class="pcHide" alt="">
+                <h2 class="u36Medium_v2">
+                    About Us
+                </h2>
+                <div class="flex-1"></div>
+                <p class="u17Light_v2">
+                    <span>
+                        Ride Smart, Ride Green
+                    </span>
+                </p>
+                <a href="">Learn more</a>
+            </div>
+        </div>
+    </div>
+    <div class="blogs">
+        <div class="centeral-content">
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_22789_2x_d4f4be37-892b-4b0d-9939-5dc0b089d144.jpg?v=1704528112" alt="">
+                <h3 class="u20Medium_v2">Urtopia Unveils the World’s First Smart E-Bike with ChatGPT Integration at EUROBIKE 2023</h3>
+                <a href="">Read more</a>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_22789_2x_d4f4be37-892b-4b0d-9939-5dc0b089d144.jpg?v=1704528112" alt="">
+                <h3 class="u20Medium_v2">Urtopia Unveils the World’s First Smart E-Bike with ChatGPT Integration at EUROBIKE 2023</h3>
+                <a href="">Read more</a>
+            </div>
+            <div>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_22789_2x_d4f4be37-892b-4b0d-9939-5dc0b089d144.jpg?v=1704528112" alt="">
+                <h3 class="u20Medium_v2">Urtopia Unveils the World’s First Smart E-Bike with ChatGPT Integration at EUROBIKE 2023</h3>
+                <a href="">Read more</a>
+            </div>
+            <div class="gotooooooo">
+                <span class="u20Medium_v2">To The Blog</span>
+                <img src="https://cdn.shopify.com/s/files/1/0633/2068/6808/files/Group_23561_2x_128577ad-cda4-49f4-9ce6-16f8947341fb.png?v=1704528452" alt="">
+            </div>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App53.vue'
+import App from './apps/App54.vue'
 
 createApp(App).mount('#app')

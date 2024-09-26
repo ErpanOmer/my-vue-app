cb10f824e699722b2c51f5b5328577e125cc023e
ErpanOmer <354073823@qq.com>
2024-09-26 17:26:11 +0800

    update: 更新vue

---
+++ src/apps/App64.vue
@@ -0,0 +1,94 @@
+<style lang="scss">
+@import '../styles/homepage-new.scss';
+</style>
+
+<template>
+    <div class="banner">
+        <div class="centeral-content">
+            <h1 class="u60DemiBold_v2">Carbon 1 Pro</h1>
+            <p class="u20Medium_v2">
+                <span>
+                    Full carbon fiber｜50% lighter｜30% more range
+                </span>
+            </p>
+            <a class="my-button my-button-black" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Shop now</a>
+        </div>
+    </div>
+    <div class="centeral-content your">
+        <h2 class="u36DemiBold_v2">Your Utopia is Here</h2>
+        <p class="u17Light_v2">
+            <span>
+                We strive to meet all user needs across various riding scenarios with our lightweight, smart carbon fiber electric bikes.
+            </span>
+        </p>
+        <a class="my-button my-button-black" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">explore our ebikes</a>
+    </div>
+    <div class="centeral-content cards">
+        <div>
+            <span class="u24DemiBold_v2">Ride outdoors and <br>explore more trails</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-touring bike</a>
+        </div>
+        <div>
+            <span class="u24DemiBold_v2">Explore the <br>urban landscape</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-city bike</a>
+        </div>
+        <div>
+            <span class="u24DemiBold_v2">All-Terrain Adventure</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-gravel bikes</a>
+        </div>
+    </div>
+    <div class="centeral-content carbon">
+        <h2 class="u36DemiBold_v2">Carbon fiber expert</h2>
+        <p class="u17Light_v2">
+            <span>
+                Unleash the power of carbon fiber! As experts in the field, we deliver lightweight bikes that redefine performance and elevate your riding experience. Ride with the best!
+            </span>
+        </p>
+        <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+    </div>
+    <video class="pcHide" src="https://cdn.shopify.com/videos/c/o/v/f1037f1f4d574b1ab3aca6efe4603fb3.mp4"></video>
+    <div class="app">
+        <h2 class="u36DemiBold_v2">The bicycle with a mind</h2>
+        <p class="u17Light_v2">
+            <span>
+                Where innovation fuels greatness! Our eBikes unleash unmatched intelligence, featuring cutting-edge technology like ChatGPT. Elevate your ride—dominate the road with Urtopia!
+            </span>
+        </p>
+        <a class="my-button my-button-black" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+    </div>
+    <div class="centeral-content reviews">
+        <div>
+            <span class="u24Medium_v2">Linus Tech Tips</span>
+            <span class="u17Light_v2">15.5M Subscriber YouTuber</span>
+            <span class="flex-1"></span>
+            <span class="u36DemiBold_v2">Reviews</span>
+            <span class="u17Light_v2">True review over the web</span>
+            <a class="my-button my-button-ghost" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+        <div>
+            <!-- <span class="u24Medium_v2">Linus Tech Tips</span>
+            <span class="u17Light_v2">15.5M Subscriber YouTuber</span> -->
+            <span class="flex-1"></span>
+            <span class="u36DemiBold_v2">Sustainability</span>
+            <span class="u17Light_v2">Sustainability at the Heart of Urtopia</span>
+            <a class="my-button my-button-ghost" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+    </div>
+    <div class="testride centeral-content pcHide">
+        <div>
+            <span class="u36DemiBold_v2">Test Ride Spots</span>
+            <span class="u17Light_v2">Experience the bike in real</span>
+            <a href="/pages/test-ride" target="_blank">Learn more</a>
+        </div>
+        <div>
+            <span class="u36DemiBold_v2">Peace of Mind</span>
+            <span class="u17Light_v2">24/7 Support, Full After-Sales</span>
+            <a href="/pages/test-ride" target="_blank">Learn more</a>
+        </div>
+        <div>
+            <span class="u36DemiBold_v2">About Us</span>
+            <span class="u17Light_v2">Ride Smart, Ride Green</span>
+            <a href="/pages/test-ride" target="_blank">Learn more</a>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App63.vue'
+import App from './apps/App64.vue'
 
 createApp(App).mount('#app')

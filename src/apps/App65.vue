fe5b757ea6dac37c9bbd7735ad83867f111b78c6
ErpanOmer <354073823@qq.com>
2024-10-11 13:51:42 +0800

    update: 更新vue

---
+++ src/apps/App65.vue
@@ -0,0 +1,68 @@
+<style lang="scss">
+@import '../styles/collections.scss';
+</style>
+
+
+<template>
+    <div class="banner">
+        <div class="centeral-content">
+            <h2 class="u48DemiBold_v2">Electric Bikes</h2>
+            <h3 class="u24Medium_v2">Carbon Fiber Expert</h3>
+        </div>
+    </div>
+    <h3 class="centeral-content u36DemiBold_v2 Choose">
+        Choose your ride
+    </h3>
+    <div class="centeral-content cards">
+        <div>
+            <span class="u24DemiBold_v2">Ride outdoors and <br>explore more trails</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-touring bike</a>
+        </div>
+        <div>
+            <span class="u24DemiBold_v2">Explore the <br>urban landscape</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-city bike</a>
+        </div>
+        <div>
+            <span class="u24DemiBold_v2">All-Terrain Adventure</span>
+            <a class="my-button" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">E-gravel bikes</a>
+        </div>
+    </div>
+    <div class="centeral-content care">
+        <div style="--order: 2;">
+            <h3 class="u30Medium_v2">Urtopia CARE</h3>
+            <p class="u17Light_v2">
+                <span>
+                    Our dedicated after-sales service that ensures you can ride with complete peace of mind. From troubleshooting to regular maintenance, we’ve got you covered every step of the way.
+                </span>
+            </p>
+            <a class="my-button my-button-white" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+        <img style="--order: 1;" src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20230520-untitled-8948_2x_f6b4a856-b689-49c6-bd39-e894a356dd5b.jpg?v=1727343486" alt=""/>
+        <img style="--order: 3;" src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/20230520-untitled-8948_2x_f6b4a856-b689-49c6-bd39-e894a356dd5b.jpg?v=1727343486" alt=""/>
+        <div style="--order: 4;">
+            <h3 class="u30Medium_v2">Carbon fiber expert</h3>
+            <p class="u17Light_v2">
+                <span>Pushing the limits of what’s possible in carbon fiber bikes. With cutting-edge technology and sleek, lightweight design, we deliver an unparalleled riding experience.</span>
+            </p>
+            <a class="my-button my-button-white" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+        <div style="--order: 6;">
+            <h3 class="u30Medium_v2">The bicycle with the mind</h3>
+            <p class="u17Light_v2">
+                <span>Where innovation meets perfection. Redefining eBikes with unmatched intelligence. Packed with cutting-edge technology such as ChatGPT, our eBikes offer smart features that elevate your ride to the next level. Ride smarter, ride Urtopia!</span>
+            </p>
+            <a class="my-button my-button-white" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+        <img style="--order: 5;" src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/DSCF1581_2x_dcfecbd1-83a7-4f6c-bf7a-14d8b65e3df2.jpg?v=1727343481" alt=""/>
+        <img style="--order: 7;" src="https://cdn.shopify.com/s/files/1/0583/5810/4213/files/24751_2x_8eff0d7e-b9a8-4429-8309-772edb257630.jpg?v=1727343476" alt=""/>
+        <div style="--order: 8;">
+            <h3 class="u30Medium_v2">Test ride</h3>
+            <p class="u17Light_v2">
+                <span>
+                    Urtopia now has over 150 bike shops across Germany, including cities like Berlin, Essen, Hamburg, Cologne, Münster, and Stuttgart. Experience the perfect ride- try it and you’ll know!
+                </span>
+            </p>
+            <a class="my-button my-button-white" target="_blank" href="/products/urtopia-carbon-1-pro-ebike">Learn more</a>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
-import App from './apps/App64.vue'
+import App from './apps/App65.vue'
 
 createApp(App).mount('#app')
